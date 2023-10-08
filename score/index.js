1856174c0203f830655c73eea6b79c392713559e
Yash ved <yashved01@gmail.com>
2023-10-08 16:52:40 +0530

    new

---
+++ score/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'Portfolio.html'));
+  });
+  
+  // // Start the server
+app.listen(8000, () => {
+    console.log('Server started successfully');
+  });
